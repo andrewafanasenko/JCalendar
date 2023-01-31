979b646d1363d404030f1398b4a3628d5bf05800
Andrew Afanasenko <59081571+andrewafanasenko@users.noreply.github.com>
2023-01-31 21:12:07 +0200

    Develop (#2)
    * Task/libraries update (#1)
    
    * Libraries update, converted build.gradle files to kts
    
    * gitignore
    
    * gitignore
    
    * gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+buildscript {
+    extra.apply {
+        set("compose_ui_version", "1.3.3")
+    }
+}
+plugins {
+    id("com.android.application") version "7.4.0" apply false
+    id("com.android.library") version "7.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "JCalendar"
-include ':app'
+include(":app")
