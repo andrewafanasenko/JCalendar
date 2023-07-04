52d56f91cae2efe3704fab61c3f2c2a550a64287
Andrii Afanasenko <andrii.afanasenko@gmail.com>
2023-07-04 14:00:43 +0300

    Version increase

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -67,7 +67,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.andrewafanasenko"
             artifactId = "jcalendar"
-            version = "1.0.7"
+            version = "1.0.8"
 
             afterEvaluate {
                 from(components["release"])
